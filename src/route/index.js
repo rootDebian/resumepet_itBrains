fbeac986534531113a73b73be4913c014993abd0
Oleksii.Kryvenkov <askryvenkov@gmail.com>
2023-04-26 21:24:34 +0300

    btstrp practice

--- src/route/index.js
+++ src/route/index.js
@@ -1075,6 +1075,16 @@ router.get('/facebook', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
